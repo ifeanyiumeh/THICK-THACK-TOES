a70d4f60dba90c412c3b50d6e3ff2f410066e005
Ifeanyi Umeh <somto_umeh@yahoo.com>
2022-02-26 00:22:33 -0500

    COMPLETED THE TIC TAC TOES

---
+++ src/components/Game.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import calculateWinner from "../helper";
+import Board from "./Board";
+import "./game.css";
+export const Game = () => {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [stepNumber, setStepNumber] = useState(0);
+  const [xIsNext, setXisNext] = useState(true);
+  const winner = calculateWinner(history[stepNumber]);
+  const xO = xIsNext ? "X" : "O";
+
+  const handleClick = (i) => {
+    const historyPoint = history.slice(0, stepNumber + 1);
+    const current = historyPoint[stepNumber];
+    const squares = [...current];
+
+    //CHEKS FOR WINER SO X  DOESNT REPLACE O or VIce Versa
+    if (winner || squares[i]) return;
+
+    //SLELCT SQUARES
+    squares[i] = xO;
+    setHistory([...historyPoint, squares]);
+    setStepNumber(historyPoint.length);
+    setXisNext(!xIsNext);
+  };
+
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXisNext(step % 2 === 0);
+  };
+
+  const renderMoves = () =>
+    history.map((_step, move) => {
+      const destination = move ? `Go to move #${move}` : "Go to start";
+      return (
+        <li key={move}>
+          <button onClick={() => jumpTo(move)}>{destination}</button>
+        </li>
+      );
+    });
+
+  return (
+    <>
+      <h1>REACT TAC TOES - Proudly sponsored by React Hoooks</h1>
+      <Board squares={history[stepNumber]} onClick={handleClick} />
+      <div className="info-wrapper">
+        <div>
+          <h3>Traverse History Below</h3>
+          {renderMoves()}
+        </div>
+        <h3>{winner ? "winner " + winner : "Next Player: " + xO}</h3>
+      </div>
+    </>
+  );
+};
---
+++ src/components/Square.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Square({ value, onClick }) {
+  const style = value ? `Squares ${value}` : "squares";
+
+  return (
+    <button className={style} onClick={onClick}>
+      {value}
+    </button>
+  );
+}
